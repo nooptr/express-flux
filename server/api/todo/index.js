598671056b358e4bd9ef5b5dd6c2c847771f6c54
Ngo Van Thang <thangnvbkhn@gmail.com>
2016-04-03 23:05:01 +0900

    Fix error: No 'Access-Control-Allow-Origin' header is present on the requested resource

--- server/api/todo/index.js
+++ server/api/todo/index.js
@@ -2,6 +2,12 @@ var express = require('express'),
     router = express.Router(),
     controller = require('./todo.controller');
 
+// For Cross Origin
+router.all( '/*', function ( req, res, next ) {
+    res.contentType( 'json' );
+    res.header( 'Access-Control-Allow-Origin', '*' );
+    next();
+});
 router.get('/', controller.getAll);
 router.post('/create', controller.create);
 router.post('/update', controller.update);
--- server/routes/restapi.js
+++ server/routes/restapi.js
@@ -1,10 +1,3 @@
 module.exports = function(app) {
     app.use('/api/todo', require('../api/todo'));
-
-    // For Cross Origin
-    app.route('/*')
-        .get(function(req, res) {
-            res.contentType('json');
-            res.send({status: "error", message: "end point not found"});
-        });
 };
